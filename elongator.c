e8489a55718b6764d57afa36ff05a4eb73c160a1
vishnu shashank <shashankmv01@gmail.com>
2015-12-31 23:52:46 +0530

    Update elongator.c

--- elongator.c
+++ elongator.c
@@ -11,23 +11,23 @@
  
     fp1 = fopen("D:/x.mp3","rb");    
     fp2 = fopen("D:/y.mp3", "wb");
-    fseek(fp1,0,SEEK_END);
-    pos=ftell(fp1);          // gives number of characters present in the input file
-    fseek(fp1,0,SEEK_SET);
+    fseek(fp1,0,SEEK_END);					// setting pointer to end of the input file
+    pos=ftell(fp1);							// gives number of characters present in the input file
+    fseek(fp1,0,SEEK_SET);					// setting pointer to the start of the input file
     while (pos--)
     {
-        ch = fgetc(fp1);  // copying file character by character
+        ch = fgetc(fp1);					// copying file character by character
         fputc(ch, fp2);
     }    
-    fclose(fp1);
-    fp1 = fopen("D:/z.mp3","rb");           
+    fclose(fp1);							// closing x.mp3
+    fp1 = fopen("D:/z.mp3","rb"); 			// opening z.mp3      
     fseek(fp1,0,SEEK_END);
     pos=ftell(fp1); 
     fseek(fp1,0,SEEK_SET);
     fseek(fp2,0,SEEK_END);
     while (pos--)
     {
-        ch = fgetc(fp1);  // copying file character by character
+        ch = fgetc(fp1); 					 // copying file character by character
         fputc(ch, fp2);
     } 
 	fclose(fp1);  
