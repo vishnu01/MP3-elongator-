d183c629b8ac71dc9fc19065885cb0a9f30bc92b
vishnu shashank <shashankmv01@gmail.com>
2016-01-01 00:24:55 +0530

    Update elongator.c

--- elongator.c
+++ elongator.c
@@ -1,36 +1,34 @@
-#include<stdio.h>
 
- main()
-{   
-    /*
-     two songs named x and y ( in the d drive ) are elongated and copied into the third file named y.mp3
+/*
+     sample two songs (a.mp3 and b.mp3) are extended and the ouput file is named as result.mp3
     */
-    FILE *fp1, *fp2;
+#include<stdio.h>
+void extend(FILE *fp)
+{
+	FILE *fp2;
     char ch;
-    int pos;
- 
-    fp1 = fopen("D:/x.mp3","rb");    
-    fp2 = fopen("D:/y.mp3", "wb");
-    fseek(fp1,0,SEEK_END);					// setting pointer to end of the input file
-    pos=ftell(fp1);							// gives number of characters present in the input file
-    fseek(fp1,0,SEEK_SET);					// setting pointer to the start of the input file
-    while (pos--)
-    {
-        ch = fgetc(fp1);					// copying file character by character
-        fputc(ch, fp2);
-    }    
-    fclose(fp1);							// closing x.mp3
-    fp1 = fopen("D:/z.mp3","rb"); 			// opening z.mp3      
-    fseek(fp1,0,SEEK_END);
-    pos=ftell(fp1); 
-    fseek(fp1,0,SEEK_SET);
+    int pos; 
+    fp2 = fopen("D:/result.mp3", "wb+");
+    fseek(fp,0,SEEK_END);					// setting pointer to end of the input file
+    pos=ftell(fp);							// gives number of characters present in the input file
+    fseek(fp,0,SEEK_SET);					// setting pointer to the start of the input file
     fseek(fp2,0,SEEK_END);
     while (pos--)
     {
-        ch = fgetc(fp1); 					 // copying file character by character
+        ch = fgetc(fp);					// copying file character by character
         fputc(ch, fp2);
-    } 
+    }    
+    fclose(fp);							// closing .mp3
+	fclose(fp2);	
+}
+
+ main()
+{   
+    
+    FILE *fp1;
+    extend(fp1=fopen("D:/a.mp3","rb"));  
+	fclose(fp1);
+	extend(fp1=fopen("D:/b.mp3","rb"));
 	fclose(fp1);  
-    fclose(fp2);    
     return 0;
 }
